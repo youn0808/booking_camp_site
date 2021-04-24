9d6705b2852860f8ea341bae58acace4cc623127
youn <youns@myumanitoba.ca>
2021-04-24 02:28:57 -0500

    apply bootstrap

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     tittle:String,
-    price:String,
+    image:String,
+    price:Number,
     description:String,
     location:String
 });
--- seeds/index.js
+++ seeds/index.js
@@ -15,11 +15,16 @@ const sample = array =>array[Math.floor(Math.random()*array.length)];
 const seedDB = async()=>{
     await Campground.deleteMany({});
     
-    for(let i = 0; i<50; i++){
+    for(let i = 0; i<10; i++){
         const random1000 =Math.floor(Math.random()*1000);
+        const randomPrice =Math.floor(Math.random()*50)+10;
         const camp = new Campground({
             tittle:`${sample(places)} in ${sample(descriptors)}`,
-            location:`${cities[random1000].city} - ${cities[random1000].state} `
+            location:`${cities[random1000].city} - ${cities[random1000].state} `,
+            image:'https://source.unsplash.com/collection/10019480',
+            description:'description',
+            price:randomPrice
+
         })
         await camp.save();
     }
